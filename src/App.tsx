72d58bd66e9adae9fa890459a80b764c5d78dd83
SergeiZimin <sergej.zimin.97@bk.ru>
2023-12-06 23:29:38 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Layout} from './components/layout/Layout';
+
+const App: React.FC = () => {
+  return (
+    <Layout>
+      <div>
+      </div>
+    </Layout>
+  );
+};
+
+export default App;
